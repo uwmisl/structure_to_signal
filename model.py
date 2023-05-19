5e0f09d340110e7aee8500adc90443fa9126b709
ashleystephenson <40045365+ashleystephenson@users.noreply.github.com>
2023-05-18 20:01:00 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,118 +1,121 @@
 import torch
 import torch.nn as nn
 from torch_geometric.nn import GCNConv
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
 import torch.nn.functional as F
 from dataset import DatasetLoader
+import os, glob
+from matplotlib import pyplot as plt
+import numpy as np
 
-# Paper model notes:
-# GCN layers with exponential linear unit (ELU) activation
-# Standard 1D CNN layers with relu activation 
-    # average pooling of 2x2 patches of each CNN layer output matrix
-# Flatten layer, converts final 1D CNN output matrix to 1D feature vector in row-wise fashion
-# Input to NN layer then combine elements of NN layer output vector linearly to produce current value
+def rmse(y, y_pred):
+    return np.sqrt(np.mean(np.square(y - y_pred)))
 
-# "We used the following scaling factor to determine the number of nodes in each GCN/CNN layer of our framework:
-# 𝑛=16×2(𝑙−1),
-# where l is the layer index of the GCN, CNN, and NN layer groups. For instance, the number of GCN layers 
-# determined to yield the best performance for DNA was 4. The number of nodes for each GCN layer was 
-# therefore 128, 64, 32, and 16. The same logic was applied to all other layer groups."
+def plot_train_test(epochs, train_losses, test_losses):
+    plt.plot(list(range(epochs)), train_losses, label="Train")
+    plt.plot(list(range(epochs)), test_losses, label="Test")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
 
-# e.g. "The optimal framework for DNA analysis (ATGC DNA) has four GCN layers and three CNN layers with a 
-# kernel size of 10 and 8192 nodes in the NN layer"
-
-# Something simple to get training loop tested but somewhat random for now 
-class SimpleNetwork(nn.Module):
+class SimpleGNN(nn.Module):
     def __init__(self):
-        super(SimpleNetwork, self).__init__()
-        num_features = 8 # x = [2926, 8] = [nodes, features] so num_features = 8? 
-        self.GCNconv1 = GCNConv(num_features, 64) 
-        self.GCNconv2 = GCNConv(64, 32)
-        self.conv1 = nn.Conv1d(32, 16, 3)
-        self.fc1 = nn.Linear(16, 22) # num_classes = 22
+        super(SimpleGNN, self).__init__()
+        torch.manual_seed(42)
 
-    def forward(self, x, edge_index):
-        x, edge_index = data.x, data.edge_index
-        x = F.elu(self.GCNconv1(x, edge_index))
-        x = F.elu(self.GCNconv2(x, edge_index))
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = self.fc1(x)
-        return x
+        self.conv1 = GCNConv(num_features, embedding_size) 
+        self.conv2 = GCNConv(embedding_size, embedding_size)
+        self.conv3 = GCNConv(embedding_size, embedding_size)
+        self.conv4 = GCNConv(embedding_size, embedding_size)
+        self.out = nn.Linear(embedding_size*2, 1) 
 
-# Trying to replicate "optimal" layer numbers/sizes from ref. paper but we should implement parameter seach function
-# class Network(nn.Module):
-#     def __init__(self):
-#         super(Network, self).__init__()
-#         self.GCNconv1 = GCNConv(num_features, 128)
-#         self.GCNconv2 = GCNConv(128, 64)
-#         self.GCNconv3 = GCNConv(32, 16)
-#         self.GCNconv4 = GCNConv(16, )
-#         self.conv1 = nn.Conv1d( , , 10)
-#         self.conv2 = nn.Conv1d( , , 10)
-#         self.conv3 = nn.Conv1d( , 8192, 10)
-#         self.fc1 = nn.Linear(8192, 22) # num_classes = 22
+    def forward(self, x, edge_index, batch_index):
+        h = self.conv1(x, edge_index)
+        h = F.relu(h)
+        h = self.conv2(h, edge_index)
+        h = F.relu(h)
+        h = self.conv3(h, edge_index)
+        h = F.relu(h)
+        h = self.conv4(h, edge_index)
+        h = F.relu(h)
+          
+        # Global Pooling (stack mean pooling and max pooling)
+        h = torch.cat([gmp(h, batch_index), 
+                            gap(h, batch_index)], dim=1)
 
-#     def forward(self, x, edge_index):
-#         x, edge_index = data.x, data.edge_index
-#         x = F.elu(self.GCNconv1(x, edge_index))
-#         x = F.elu(self.GCNconv2(x, edge_index))
-#         x = F.elu(self.GCNconv3(x, edge_index))
-#         x = F.elu(self.GCNconv4(x, edge_index))
-#         x = F.relu(self.conv1(x))
-#         x = F.relu(self.conv2(x))
-#         x = F.relu(self.conv3(x))
-#         x = self.fc1(x)
-#         return x
+        # Output layer
+        out = self.out(h)
 
+        return out, h
 
-def train(epochs):
+def train(train_loader):#, model, optimizer, criterion):
     model.train()
-    train_losses = []
-    train_accs = []
-    for epoch in range(epochs):
+    for train_data in train_loader:
         # zero gradients
         optimizer.zero_grad()
         # Forward pass
-        y_pred = model(data.x, data.edge_index)
+        y_pred, embedding = model(train_data.x, train_data.edge_index.long(), train_data.batch)
         # Calculate loss
-        loss = criterion(y_pred, data.y)
-        # Calculate accuracy
-        # acc = 
+        # print(f'y: {train_data.y.shape} pred: {y_pred.shape}')
+        loss = criterion(y_pred, train_data.y.unsqueeze(1))
         # Compute gradients
         loss.backward()
         # Update parameters
         optimizer.step()
-        
-        train_losses.append(loss)
-        train_accs.append(acc)
-    return train_losses, train_accs
+    return loss, embedding
 
 @torch.no_grad()
-def test():
+def test(test_loader):#, model, optimizer, criterion, epochs):
     model.eval()
-    val_losses = []
-    val_accs = []
-    y_pred = model()
-    loss = criterion(y_pred, data.y)
-    # acc = 
+    for test_data in test_loader:
+        y_pred, embedding = model(test_data.x, test_data.edge_index.long(), test_data.batch)
+        loss = criterion(y_pred, test_data.y.unsqueeze(1))
+    return loss, embedding        
 
 
-# implement grid search for optimal hyperparameters (nummber of GNN, CNN, NN layers, CNN kernel size)
-# def tune_model():
-#     return history
+# __________________________________________________________________________________________
+# Load data
+file_list = glob.glob('data/processed/data_*')
+examples = [torch.load(f) for f in file_list]
 
+# Train/test split
+train_size = int(0.7 * len(examples))
+test_size = len(examples) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(examples, [train_size, test_size])
+print('num training examples: ', len(train_dataset))
+print('num testing examples: ', len(test_dataset))
 
-data = torch.load('data/processed/data.pt')
+# Set up data loaders
+num_graphs_per_batch = 5
+train_loader = DataLoader(train_dataset, batch_size=num_graphs_per_batch, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=num_graphs_per_batch, shuffle=True)
+
+# Load model and set parameters
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(data)
-print(data[0])
+embedding_size = 64 # GCNConv size
+num_features= 8
+model = SimpleGNN().to(device) 
+model = model.double()
+print(model)
+print("Parameters: ", sum(p.numel() for p in model.parameters()))
 
-
-model = SimpleNetwork().to(device) 
-
-optimizer_name = "Adam"
-lr = 1e-1
-optimizer = getattr(torch.optim, optimizer_name)(model.parameters(), lr=lr)
+optimizer = "Adam"
+lr = 0.0001
+optimizer = getattr(torch.optim, optimizer)(model.parameters(), lr=lr)
 criterion = nn.MSELoss()
-epochs = 50
+epochs = 30
 
+# Train model
+train_losses = []
+test_losses = []
+for epoch in range(epochs):
+    trn_loss, trn_emb = train(train_loader)
+    train_losses.append(trn_loss.item())
+    tst_loss, tst_emb = test(test_loader)
+    test_losses.append(tst_loss.item())
+    print(f'Epoch: {epoch}, Train Loss: {trn_loss:.4f}, Test Loss: {tst_loss:.4f}')
+
+# Plot
+plot_train_test(epochs, train_losses, test_losses)
